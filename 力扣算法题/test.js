edc52918d689959c6350f33d78da6daf73242d5f
阳九 <871921282@qq.com>
2023-06-29 14:24:04 +0800

    复习算法  检验二叉搜索树

--- 力扣算法题/test.js
+++ 力扣算法题/test.js
@@ -1,86 +1,17 @@
+// 通过event实现一个简单的Promise/A 规范
+class Promise extends EventEmitter {
+    constructor() { }
 
-const times = 5_000_000_000
-
-function process() {
-    return 1 + 2
-}
-function daff_1(times) {
-
-    let iterations = Math.floor(times / 32);
-    let leftover = times % 32;
-    // 循环第一部分
-    if (leftover > 0) {
-        do {
-            process();
-        } while (--leftover > 0);
+    // 利用EventEmitter.once实现一次回调执行
+    then(resolve, reject, process) {
+        this.once("resolve", resolve)
+        this.once("reject", reject)
+        this.once("process", process)
     }
-    // 循环第二部分 8次为一组(do while比普通的while和for更快  因为少一次初始条件判断)
-    do {
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-        process();
-    } while (--iterations > 0);
 
+    all(promiseArr) {
+        promiseArr.forEach(p => {
+            // ...
+        })
+    }
 }
-function daff_2(times) {
-    let iterations = Math.ceil(times / 8);
-    let startAt = times % 8;
-    do {
-        switch (startAt) {
-            case 0: process();
-            case 7: process();
-            case 6: process();
-            case 5: process();
-            case 4: process();
-            case 3: process();
-            case 2: process();
-            case 1: process();
-        }
-        startAt = 0;
-    } while (--iterations > 0);
-}
-
-
-console.time("For Iterator");
-for (let i = 0; i < times; i++) {
-    process()
-}
-console.timeEnd("For Iterator");
-
-console.time("Daff Iterator");
-daff_2(times)
-console.timeEnd("Daff Iterator");
-
-console.time("Daff Iterator+");
-daff_1(times)
-console.timeEnd("Daff Iterator+");
-
